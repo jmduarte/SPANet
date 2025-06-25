444e30071fdd7a07496631162388abf89278c050
Javier Duarte <jduarte@ucsd.edu>
2025-06-25 15:03:12 -0700

    Update version to 2.3 in setup.py

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ long_description = (this_directory / "README.md").read_text()
 setup(
     name="spanet",
     packages=find_packages(),
-    version="2.2.0",
+    version="2.3.0",
     description="Symmetry Preserving Attention Networks",
     long_description=long_description,
     long_description_content_type='text/markdown',
